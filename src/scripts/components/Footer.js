6ff8a81cfec8a301e2420eea930037270007bdaf
Saurabh Chaudhary <sauchau0@publicisgroupe.net>
2018-07-17 17:47:38 +0530

    code changes

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
 
 export default class Footer extends Component {
     render() {
         return (
             <div className=" container footer-container">
-                <a className="btn nav-btn">HOME</a>
-                <a className="btn nav-btn">PRODUCTS</a>
-                <a className="btn nav-btn">THEMES</a>
-                <a className="btn nav-btn">TEAMS</a>
+                <a className="btn nav-btn" href="/">
+                    HOME
+                </a>
+                <a className="btn nav-btn" href="/products.html">
+                    PRODUCTS
+                </a>
+                <a className="btn nav-btn" href="/themes.html">
+                    THEMES
+                </a>
+                <a className="btn nav-btn" href="/teams.html">
+                    TEAMS
+                </a>
                 <a className="btn nav-btn">QUICK LINKS</a>
                 <a className="btn nav-btn">MY FAVORITES</a>
             </div>
